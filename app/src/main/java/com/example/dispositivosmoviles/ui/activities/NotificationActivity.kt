e8523d79a99db942163a10b2ab12c814992b20cd
Fernando Mosquera <luismosquera97@gmail.com>
2023-07-30 21:27:37 -0500

    notificaciones

--- app/src/main/java/com/example/dispositivosmoviles/ui/activities/NotificationActivity.kt
+++ app/src/main/java/com/example/dispositivosmoviles/ui/activities/NotificationActivity.kt
@@ -1,12 +1,83 @@
 package com.example.dispositivosmoviles.ui.activities
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.dispositivosmoviles.R
+import com.example.dispositivosmoviles.databinding.ActivityNotificationBinding
 
 class NotificationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNotificationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notification)
+
+        binding = ActivityNotificationBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        binding.btnNotification.setOnClickListener(){
+
+            createNotification()
+            sendNotificaction()
+
+
+        }
     }
+
+    val CHANNEL: String = "Cotificacion"
+
+    private fun createNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Variedades"
+            val descriptionText = "Notificacion simples de variables"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun sendNotificaction(){
+
+        val intent = Intent(this, CameraActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+
+        val noti= NotificationCompat.Builder(this, CHANNEL)
+        noti.setContentTitle("Primera notificacion")
+        noti.setContentText("MI primera notificaciones en android")
+        noti.setSmallIcon(R.drawable.home_48)
+        noti.setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        noti.setStyle(
+            NotificationCompat
+                .BigTextStyle()
+                .bigText("Esta es una notificacion para recordar que esta es mi primera implementacipn de notificacion"))
+
+        noti.setContentIntent(pendingIntent)
+
+        with(NotificationManagerCompat.from(this)){
+            notify(1,noti.build())
+        }
+
+}
 }
